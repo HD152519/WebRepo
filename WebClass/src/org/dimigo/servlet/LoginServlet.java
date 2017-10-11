8eaf64d003fe7e3a29af6960a11af2dae370e1c0
shinjuyoung <shinjuyoung@218.38.137.28>
2017-10-11 21:30:35 +0900

    제

--- WebClass/src/org/dimigo/servlet/LoginServlet.java
+++ WebClass/src/org/dimigo/servlet/LoginServlet.java
@@ -5,11 +5,15 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.dimigo.vo.UserVO;
 
 import com.google.gson.Gson;
 
@@ -32,14 +36,49 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//jsp 포워딩
+		RequestDispatcher rd = request.getRequestDispatcher("jsp/login.jsp"); 
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String id=request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		System.out.printf("id : %s, pwd: %s\n",id,pwd);
+		
+		//id, pwd 접합성 체크 
+		boolean result = false;
+		
+		if(result) {
+			//세션에 사용자 정보를 생성해서 담기 
+			HttpSession session = request.getSession();
+			UserVO user = new UserVO();
+			user.setId(id);
+			user.setName("홍길동");
+			user.setNickname("의적");
+			
+			RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp"); 
+			rd.forward(request, response);
+			
+			
+			session.setAttribute("user",user);
+			
+		}else {
+			request.setAttribute("msg", "error");
+			RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp"); 
+			rd.forward(request, response);
+		}
+      
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
 		String id=request.getParameter("id");
